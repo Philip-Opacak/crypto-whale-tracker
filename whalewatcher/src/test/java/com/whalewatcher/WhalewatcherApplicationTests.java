e46a797c3bd1aee2e3c2a9b2ffda86c487de620e
Philip Opacak <philipopacak@outlook.com>
2025-11-11 19:50:57 -0700

    Initial commit

---
+++ whalewatcher/src/test/java/com/whalewatcher/WhalewatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whalewatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalewatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
